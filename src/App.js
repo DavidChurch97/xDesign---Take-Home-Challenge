7e9330a0f8c6dcd62beef979479f0379c34e573f
David <davidchurch97@gmail.com>
2020-12-09 15:55:27 +0000

    Added css design for launch list and learning beginning to learn apis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import logo from './assets/spacex-logo.png';
 import './App.css';
+import React, { useState } from "react";
+
+import refreshIcon from './assets/icon/refresh.png';
+import yearSelectIcon from './assets/icon/select.png';
+import sortIcon from './assets/icon/sort.png';
+import bodyImage from './assets/img/launch-home.png';
+
 
 function App() {
+  const [launch, setLaunch] = useState("");
+
+  const getLaunch = () => {
+    fetch("https://api.spacexdata.com/v3/launches")
+      .then((response) => response.json())
+      .then((data) => {
+        setLaunch(data.flgiht_number + "..." + data.launch);
+      });
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-logo-subText">LAUNCHES</p> 
+
+        <button className="refreshBtn">Reload Data 
+        <img src={refreshIcon} className="refreshBtn-Icon" alt="refreshIcon" />
+        </button> 
       </header>
+
+      <div className="container">
+        <img src={bodyImage} className="bodyImage" alt="bodyImage" />
+
+        <div className="data">
+          <button className="yearFilter">Filter By Year
+            <img src={yearSelectIcon} className="yearSelectIcon" alt="refreshIcon" />
+          </button>
+
+          <button className="sortList">Sort Descending
+            <img src={sortIcon} className="sortIcon" alt="refreshIcon" />
+          </button>
+
+          <div className="launchList" onLoad={getLaunch}>
+            {launch}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
